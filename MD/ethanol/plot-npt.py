44c4b6a5645ada94d9e23bd213932b1513833f6d
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2022-10-26 14:53:09 -0400

    Update plot-npt

--- MD/ethanol/plot-npt.py
+++ MD/ethanol/plot-npt.py
@@ -1,5 +1,7 @@
-# Plotting script for LAMMPS property file
-# For NPT equilibration run
+"""
+Plotting script for LAMMPS property file
+for NPT equilibration run
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,7 +17,7 @@ import json
 data_npt = np.genfromtxt("properties.npt.dat")
 
 # The column order is defined in the lammps input
-# file. Python indexing starts with 0. That means, 
+# file. Python indexing starts with 0. That means,
 # that the first column is refered as the zeroth
 # column
 
@@ -33,26 +35,27 @@ ax.spines["left"].set_linewidth(3)
 ax.spines["right"].set_linewidth(3)
 ax.spines["top"].set_linewidth(3)
 
-#ax.grid()
-#ax.title.set_text('Ethanol density at 298K')
-ax.set_xlabel(r'Timestep')
-ax.set_ylabel('Density $(g/cm^3)$')
+
+half_idx = len(density_npt) // 2
+equil_density = density_npt[half_idx:]
+equil_timestep = timestep[half_idx:]
+mean_density = np.round(np.mean(equil_density), 4)
+# The number 0.7873 is taken from the paper shared
+# in class
+mean_reference = 0.7873
+
+ax.title.set_text(f"Ethanol density at 298K. Mean: {mean_density} $g/cm^3$")
+ax.set_xlabel(r"Timestep")
+ax.set_ylabel("Density $(g/cm^3)$")
 ax.yaxis.tick_left()
-#ax.set_xlim([-1500, 300])
-#ax.set_ylim([220, 350])
-ax.plot(timestep, density_npt, '-', color="black", label="NPT")
-npt_last_timestep = timestep[-1]
 
+ax.plot(timestep, density_npt, "-", color="black", label="NPT")
+ax.plot(equil_timestep, equil_density, "-", color="blue", label="Equilibrated NPT")
 
-# The number 0.7873 is taken from the paper shared                                                                    
-# in class                                                                                                            
-ax.axhline(y=0.7873, color='r', linestyle='-', label='Reference')
+ax.axhline(y=mean_density, color="b", linestyle="-", label="Mean Equilibrated NPT")
+ax.axhline(y=mean_reference, color="r", linestyle="-", label="Reference")
 
 ax.legend(loc="best")
 ax.yaxis.set_label_position("left")
-
-annotation = "Figure 1. Ethanol density at 298K"
-plt.annotate(annotation, (0,0), (-150, -40), xycoords='axes points', textcoords='offset points', va='top')
-
 
 plt.show()
